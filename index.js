0862e9164eea733eb4e9412be606a94a37c8edf9
dyo1319 <raad_asded@hotmail.com>
2025-06-25 02:39:36 +0300

    Implement EJS logic to render 10-row table on index page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//npm i express body-parser / ejs
 const port = 8888;
 const express = require('express');
 const app = express();
@@ -8,3 +8,16 @@ app.use(express.json());
 const bodyParser = require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"./views"));
+
+
+app.get('/', (req, res) => {
+    res.render("index", {});
+})
+
+
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
+});
