821a8778f9b46c935c28ca7cd3051d452508ff71
dyo1319 <raad_asded@hotmail.com>
2025-07-30 17:47:48 +0300

    front-end page to add course

--- routers/course_R.js
+++ routers/course_R.js
@@ -4,6 +4,10 @@ module.exports = router;
 
 const course_Mid = require('../middleware/course_Mid');
 
+router.get("/Add", (req, res) => {
+    res.render("crs_add", {});
+});
+
 router.post("/Add", [course_Mid.AddCourse], (req,res) => {
      res.send("Course added successfully");
-})
+});
