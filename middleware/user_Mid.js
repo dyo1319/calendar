e26209854a8bd37ce75e8183ce5d277e16bc7a08
dyo1319 <raad_asded@hotmail.com>
2025-07-31 18:06:40 +0300

    encrypt passwd

--- middleware/user_Mid.js
+++ middleware/user_Mid.js
@@ -1,9 +1,27 @@
 const { addSlashes } = require("slashes");
+const { md5 } = require("md5");
+
+async function CheckLogin(uname, passwd) {
+    let enc_pass = md5("A"+passwd);
+    let Query = `SELECT * FROM users WHERE uname='${uname}' AND passwd='${enc_pass}'`;
+
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+    } catch (err) {
+        console.log(err);
+    }
+
+    return (rows.length > 0);
+}
+
 
 async function AddUser(req,res,next) {
     let name    = (req.body.name    !== undefined)  ? addSlashes(req.body.name       ) : "";
     let uname   = (req.body.uname   !== undefined)  ? addSlashes(req.body.uname      ) : "";
-    let passwd  = (req.body.passwd  !== undefined)  ? addSlashes(req.body.passwd     ) : "";
+    let passwd  = (req.body.passwd  !== undefined)  ?     req.body.passwd              : "";
+    let enc_pass = md5("A"+passwd);
     let email   = (req.body.email   !== undefined)  ? addSlashes(req.body.email      ) : "";
     let type_id = (req.body.type_id !== undefined)  ?     Number(req.body.type_id    ) : -1;
     let tz      = (req.body.tz      !== undefined)  ? addSlashes(req.body.tz         ) : "";
@@ -11,7 +29,7 @@ async function AddUser(req,res,next) {
     let Query ="INSERT INTO users";
     Query += "( `name`, `uname`, `passwd`, `email`, `type_id`, `tz`)";
     Query += " VALUES ";
-    Query += `('${name}', '${uname}', '${passwd}', '${email}', ${type_id}, '${tz}')`;
+    Query += `('${name}', '${uname}', '${enc_pass}', '${email}', ${type_id}, '${tz}')`;
     
     const promisePool = db_pool.promise();
     let rows=[];
@@ -27,7 +45,7 @@ async function UpdateUser(req,res,next) {
     let id = parseInt(req.params.id);
     let name    = (req.body.name    !== undefined)  ? addSlashes(req.body.name       ) : "";
     let uname   = (req.body.uname   !== undefined)  ? addSlashes(req.body.uname      ) : "";
-    let passwd  = (req.body.passwd  !== undefined)  ? addSlashes(req.body.passwd     ) : "";
+    // let passwd  = (req.body.passwd  !== undefined)  ? addSlashes(req.body.passwd     ) : "";
     let email   = (req.body.email   !== undefined)  ? addSlashes(req.body.email      ) : "";
     let type_id = (req.body.type_id !== undefined)  ?     Number(req.body.type_id    ) : -1;
     let tz      = (req.body.tz      !== undefined)  ? addSlashes(req.body.tz         ) : "";
@@ -41,7 +59,7 @@ async function UpdateUser(req,res,next) {
     let Query =`UPDATE users SET `;
      Query += `name    ='${name   }'  ,`; 
      Query += `uname   ='${uname  }'  ,`; 
-     Query += `passwd  ='${passwd }'  ,`; 
+    //  Query += `passwd  ='${passwd }'  ,`; 
      Query += `email   ='${email  }'  ,`; 
      Query += `type_id ='${type_id}'  ,`; 
      Query += `tz      ='${tz     }' `; 
@@ -121,5 +139,6 @@ module.exports = {
     GetAllUser,
     DeleteUser,
     GetOneUser,
-    UpdateUser
+    UpdateUser,
+    CheckLogin
 }
